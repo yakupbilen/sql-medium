c4e448d595ec763b52786d3897c3ebd0ea61b17c
Yakup Bilen <bilenyakup97@gmail.com>
2022-08-13 16:50:46 +0300

    Add files via upload

---
+++ database/soccer_mysql.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:283f9987a7a673c3eaa201497111f13f7d650f26013de271159f20d6e9ce1b7e
+size 302910629
---
+++ database/soccer_postgre.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f660793a25d1203441bf0e472ecefd33f17ba95fd4411461f1df7c85670e6bd
+size 419835025
